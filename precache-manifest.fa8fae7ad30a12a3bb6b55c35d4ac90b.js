991a482e8ddfac84b26689bd916847904fed336e
Samit Barai <samit.barai@juspay.in>
2020-09-22 15:36:07 +0530

    Updates

---
+++ precache-manifest.fa8fae7ad30a12a3bb6b55c35d4ac90b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be99fbacfcdc68cce758de294b86cd51",
+    "url": "/samit-website/index.html"
+  },
+  {
+    "revision": "a1f3e608f0a330d8b3fa",
+    "url": "/samit-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16016ce2eff6d9129890",
+    "url": "/samit-website/static/js/2.7f9cf6bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samit-website/static/js/2.7f9cf6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f3e608f0a330d8b3fa",
+    "url": "/samit-website/static/js/main.b90798e2.chunk.js"
+  },
+  {
+    "revision": "700ac72ce58b630e3ffd",
+    "url": "/samit-website/static/js/runtime-main.8635d8c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samit-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
