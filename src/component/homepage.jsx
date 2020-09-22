484c69bb1232efc596860ec6c13d7a31037783f2
Samit Barai <samit.barai@juspay.in>
2020-09-23 03:03:19 +0530

    starting portfolio

--- src/component/homepage.jsx
+++ src/component/homepage.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Header from './header';
 import Body from './body';
-import { Container } from 'react-bootstrap';
+import Portfolio from './portfolio';
 // import Footer from './footer';
+import { Container } from 'react-bootstrap';
 
 export default class Homepage extends Component {
     render() {
       return (
-        <Container className = "bg bg-light container-fluid">
+        <Container className = "container">
             <Header />
             <Body />
+            <Portfolio />
             {/* <Footer /> */}
         </Container>
       )
---
+++ src/component/portfolio.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div className = "container-fluid">
+              <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg" alt = "" className = "rounded" />
+            </div>
+         );
+    }
+}
+ 
+export default Portfolio;
